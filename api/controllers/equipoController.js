fd2ead3df8396a7169b4941c813e1cb73de5d2f4
Roberto <roberto.arranz.93@gmail.com>
2020-04-16 19:51:45 +0200

    Dockerfile

--- api/controllers/equipoController.js
+++ api/controllers/equipoController.js
@@ -20,6 +20,7 @@ router.post('/', md_auth.ensureAuth, function(req, res) {
 
 /* GET ALL EQUIPOS */
 router.get('/', function(req, res, next) {
+    console.log(req);
     Equipo.find().populate('jugadores').sort({ orden: 1 }).exec(function(err, equipos) {
         if (err) return next(err);
         res.json(equipos);
